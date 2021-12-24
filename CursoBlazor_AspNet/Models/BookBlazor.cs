959a921238d42f984d1ebf1f950fc924201d56b3
llovo-code <lewinjoselovo@gmail.com>
2021-12-24 00:25:14 -0600

    repos

---
+++ CursoBlazor_AspNet/Models/BookBlazor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoBlazor_AspNet.Models
+{
+    public class BookBlazor
+    {
+
+        public int Id { get; set; }
+        public string bookName { get; set; }
+        public double price { get; set; }
+        public bool stock { get; set; }
+
+        public List<BookType> bookType { get; set; }
+
+    }
+}
